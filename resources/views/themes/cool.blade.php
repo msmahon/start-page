6e0e9988b17efe662ea4cec9a48606f9741be156
Mitchell Mahoney <mahoneymitch@gmail.com>
2019-01-21 18:20:07 -0500

    Placeholder theme pages.

---
+++ resources/views/themes/cool.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Cool Theme</title>
+</head>
+<body>
+    <h1>8)</h1>
+    <h2><span style="color: red;">Radical!</span> This is the <em>cool</em> theme</h2>
+</body>
+</html>
