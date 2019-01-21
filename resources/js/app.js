205b90d2687bf246393e6921ca327309ecdfa944
Mitchell Mahoney <mahoneymitch@gmail.com>
2019-01-21 18:13:25 -0500

    Adding page card as global component.

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,6 +8,7 @@
 require('./bootstrap');
 
 window.Vue = require('vue');
+// window.Moment = require('moment');
 
 /**
  * The following block of code may be used to automatically register your
@@ -21,6 +22,7 @@ window.Vue = require('vue');
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+Vue.component('page-card', require('./components/PageCard.vue').default);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
