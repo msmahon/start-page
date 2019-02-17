0c5258be02a64067e7f9250fabc03f7c8f53e901
Mitchell Mahoney <mahoneymitch@gmail.com>
2019-02-16 20:02:26 -0500

    Setting up page factory.

--- database/factories/PageFactory.php
+++ database/factories/PageFactory.php
@@ -4,6 +4,8 @@ use Faker\Generator as Faker;
 
 $factory->define(StartPage\Page::class, function (Faker $faker) {
     return [
-        //
+        'slug' => $faker->uuid,
+        'name' => $faker->company,
+        'theme' => $faker->randomElement(['cool', 'default']),
     ];
 });
