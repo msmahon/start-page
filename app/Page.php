749092aff29b15f69d1a57db9a0eb4f15c83064f
Mitchell Mahoney <mahoneymitch@gmail.com>
2019-02-16 20:01:41 -0500

    Adding model relationships.

--- app/Page.php
+++ app/Page.php
@@ -6,6 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Page extends Model
 {
+
+    public function user()
+    {
+        return $this->belongsTo('StartPage\User');
+    }
+
     public function getUrl()
     {
 
