ba79360676658c44afb592ce52b6618c0b35c3fe
Mitchell Mahoney <mahoneymitch@gmail.com>
2019-03-26 18:02:35 -0400

    Trying to use more restful routes. Adding some middleware.

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/pages', 'PageController@index')->name('my-pages');
+Route::get('/pages/create', 'PageController@create')->middleware('auth');
 Route::get('/pages/{slug}', 'PageController@show');
-Route::get('/pages/delete/{slug}', 'PageController@destroy');
-Route::post('/pages', 'PageController@store');
-Route::get('/pages/create', 'PageController@create');
+Route::get('/pages/delete/{slug}', 'PageController@destroy')->middleware('auth');
+Route::post('/pages', 'PageController@store')->middleware('auth');
