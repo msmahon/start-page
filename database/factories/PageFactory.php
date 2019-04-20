80bba21d85ae1b9ba509ebcba8007926aa8284ee
Mitchell Mahoney <mahoneymitch@gmail.com>
2019-04-20 19:07:17 -0400

    Removing generated user_id.

--- database/factories/PageFactory.php
+++ database/factories/PageFactory.php
@@ -1,12 +1,11 @@
 <?php
 
 use Faker\Generator as Faker;
+use Illuminate\Foundation\Auth\User;
 
 $factory->define(StartPage\Page::class, function (Faker $faker) {
     return [
-        'user_id' => function() {
-            return factory('StartPage\User')->create()->id;
-        },
+        // 'user_id' => $this->User->id,
         'slug' => $faker->uuid,
         'name' => $faker->domainWord,
         'theme' => $faker->randomElement(['cool', 'default']),
